3e9ec92d1017f48346c9c21cd620cf79d25bead8
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-10-19 03:06:34 +0400

    "Practiced with props, added TODOs"

--- part1/react/part1/src/App.jsx
+++ part1/react/part1/src/App.jsx
@@ -1,20 +1,36 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Hello World</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   );
 };
 
 const App = () => {
+  const name = "Peter";
+  const age = 62;
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
       <Hello />
     </div>
   );
 };
 
 export default App;
+
+/* TODO: 
+Solve the last 3 exercises to better understand props:
+
+https://react.dev/learn/passing-props-to-a-component
+
+Brush up on fragments:
+
+https://react.dev/reference/react/Fragment
+*/
